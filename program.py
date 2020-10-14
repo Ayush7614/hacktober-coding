8f7c917a29932d8b56ada7ba45f1e68f2847719f
Ayush kumar <67006255+Ayush7614@users.noreply.github.com>
2020-10-14 22:54:42 +0530

    GCD of 2 numbers

---
+++ program.py
@@ -0,0 +1,11 @@
+ #GCD of 2 numbers 
+ 
+ import math 
+  
+def gcdOfFactorial(m, n) : 
+    return math.factorial(min(m, n)) 
+  
+n = int(input("Enter an number >>>"))
+m = int(input("Enter an number >>>"))
+© 2020 GitHub, Inc.
+
